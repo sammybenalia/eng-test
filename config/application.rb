83868bf084217314131708fc9795f843ec2e8dc4
Sammy Benalia <sammy@sammybenalia.com>
2013-06-04 13:59:53 -0400

    new

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ end
 
 module Blog
   class Application < Rails::Application
- 
+    config.exceptions_app = self.routes
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
